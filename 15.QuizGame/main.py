d0bba3adfd13b91f9d5761a89df0d4c4e6c3e8ec
levy123 <levylauren909@gmail.com>
2024-06-11 11:02:28 +0100

    -add new project

---
+++ 15.QuizGame/main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    text = question["text"]
+    answer = question["answer"]
+    question_bank.append(Question(text, answer))
+
+print(question_bank)
+
+while quiz.still_has_question():
+    quiz = QuizBrain(question_bank)
+
+print("You've completed the quiz!")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
+
